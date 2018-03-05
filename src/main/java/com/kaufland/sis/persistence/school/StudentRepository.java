f1115afbd6586efdfc894408bc0828dd5bf93fea
Luca Schmitzer <luca.schmitzer@kaufland.com>
2018-03-05 14:39:56 +0100

    Initial Commit

--- src/main/java/com/kaufland/sis/persistence/school/StudentRepository.java
+++ src/main/java/com/kaufland/sis/persistence/school/StudentRepository.java
@@ -1,15 +1,13 @@
 package com.kaufland.sis.persistence.school;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
-     Student findById(Long id);
+    Student findById(Long id);
 
-     Student findByUsername(String userName);
+    Student findByUsername(String userName);
 
-     Student findByEmail(String email);
+    Student findByEmail(String email);
 
 }
